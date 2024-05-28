980b71405a6277342afa5d7864f942ce1f2a8e98
Mateusz <mateusz.k.borek@gmail.com>
2024-05-28 20:03:25 +0200

    Dodano klasę z konfiguracją gry (wymiary planszy oraz poziom trudności)

---
+++ SnakeGame/Models/GameConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace SnakeGame.Models
+{
+    public class GameConfig
+    {
+        public int AreaHeight { get; set; }
+        public int AreaWidth { get; set; }
+        public Difficulty GameDifficulty { get; set; }
+
+        public GameConfig()
+        {
+            AreaHeight = 10;
+            AreaWidth = 10;
+            GameDifficulty = Difficulty.Easy;
+        }
+    }
+
+    public enum Difficulty
+    {
+        Easy = 0, 
+        Medium = 1,
+        Hard = 2
+    }
+}
