5d9051d637bd0903cbd66374a26e8c284abcb034
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-05-28 19:35:58 +0200

    Dodano menu do gry Snake

---
+++ SnakeGame/Helpers/Option.cs
@@ -0,0 +1,19 @@
+﻿namespace SnakeGame.Helpers
+{
+    public class Option
+    {
+        public string Name { get; }
+        public Action Selected { get; }
+
+        public Option(string name, Action selected)
+        {
+            Name = name;
+            Selected = selected;
+        }
+
+        public static implicit operator Option(List<Option> options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
