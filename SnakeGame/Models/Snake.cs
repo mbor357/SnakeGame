049d766dbc0f59ad7974b57570dceff551c2556a
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-05-30 10:53:04 +0200

    Dodanie modelu reprezentującego węża oraz pixel w grze

---
+++ SnakeGame/Models/Snake.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeGame.Models
+{
+    public class Snake
+    {
+        public List<Pixel> SnakeBody { get; set; }
+
+        public Snake(int areaWidth, int areaHeight)
+        {
+            SnakeBody = new List<Pixel>();
+
+            Pixel head = new Pixel
+            {
+                X = areaWidth / 2,
+                Y = areaHeight / 2
+            };
+
+            SnakeBody.Add(head);
+        }
+    }
+}
